cc25964a9451df12ce1ff341fec122682511acdb
Reggie <jacobdyak@gmail.com>
2024-02-02 00:03:19 -0800

    Enemies attack

--- Assets/Scripts/MeleeEnemy.cs
+++ Assets/Scripts/MeleeEnemy.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class MeleeEnemy : MonoBehaviour, IEnemy
 {
     private Rigidbody2D rb;
+    [SerializeField] private Transform attackHitboxCenter;
+    [SerializeField] private GameObject meleeHitbox;
 
     [SerializeField] private int health;
 
     [SerializeField] private float speed;
+
+    [SerializeField] private int damage;
+    [SerializeField] private float attackDistance;
+    [SerializeField] private float attackWindupTime;
+    [SerializeField] private float attackTime;
+    private bool attacking;
 
     public IEnemy.entityState entityState = IEnemy.entityState.freeMove;
 
@@ -22,7 +30,19 @@ public class MeleeEnemy : MonoBehaviour, IEnemy
     {
         if(entityState == IEnemy.entityState.freeMove)
         {
-            transform.position = Vector2.MoveTowards(transform.position, PlayerSingleton.player.transform.position, Time.deltaTime * speed);
+            if(Vector2.Distance(transform.position, PlayerSingleton.player.transform.position) < attackDistance && !attacking)
+            {
+                StartCoroutine(AttackCycle());
+            }
+
+            if (attacking)
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                rb.velocity = (PlayerSingleton.player.transform.position - transform.position).normalized * speed;
+            }
         }
     }
 
@@ -41,12 +61,12 @@ public class MeleeEnemy : MonoBehaviour, IEnemy
 
     public IEnumerator ActivateStun(float secondsActive)
     {
+        rb.velocity = Vector2.zero;
         entityState = IEnemy.entityState.stun;
 
         yield return new WaitForSeconds(secondsActive);
 
         entityState = IEnemy.entityState.freeMove;
-        rb.velocity = Vector2.zero;
     }
 
     private void Die()
@@ -54,5 +74,38 @@ public class MeleeEnemy : MonoBehaviour, IEnemy
         //drop loot
 
         Destroy(gameObject);
+    }
+
+    private IEnumerator AttackCycle()
+    {
+        attacking = true;
+        Vector3 playerPos = PlayerSingleton.player.transform.position;
+        playerPos.z = 5.23f;
+
+        Vector3 objectPos = transform.position;
+        playerPos.x = playerPos.x - objectPos.x;
+        playerPos.y = playerPos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(playerPos.y, playerPos.x) * Mathf.Rad2Deg;
+        attackHitboxCenter.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+        yield return new WaitForSeconds(attackWindupTime);
+
+        meleeHitbox.SetActive(true);
+        List<Collider2D> overlapColliders = new List<Collider2D>();
+        meleeHitbox.GetComponent<CapsuleCollider2D>().OverlapCollider(new ContactFilter2D().NoFilter(), overlapColliders);
+
+        foreach (Collider2D collider in overlapColliders)
+        {
+            if (collider.GetComponent<Health>() != null)
+            {
+                collider.GetComponent<Health>().Damage(damage);
+            }
+        }
+
+        yield return new WaitForSeconds(attackTime);
+
+        attacking = false;
+        meleeHitbox.SetActive(false);
     }
 }
